bcd6cc3238e00b39ef174fe7dc4f2e08b89aada1
JakubCukrowski <j.d.cukrowski@gmail.com>
2023-05-06 17:39:12 +0200

    Resolved bug with adding reply with the same name as comment

--- src/functions/CreateReplyToReply.ts
+++ src/functions/CreateReplyToReply.ts
@@ -1,4 +1,6 @@
 import { loggedUser } from "../components/Comments"
+import { commentsToObject } from "../index"
+import { submitReply } from "./SubmitReply"
 
 export const createReplyToReply = (e: Event) => {
     const replyLi = e.currentTarget as Element
@@ -8,9 +10,17 @@ export const createReplyToReply = (e: Event) => {
         //catch replies container, parent element of event target to append the reply window to it
         const repliesContainer = replyLi.parentElement as HTMLUListElement
         
+        //li of a comment 
+        const commentLi: HTMLLIElement = repliesContainer.closest("li")
+
+        //filter all comments by id
+        const commentToReply = commentsToObject.filter(comment => commentLi.id === `comment-${comment.id}`)
+        
+        const replyRef = commentToReply[0].replies.filter(reply => replyLi.id === `reply-${reply.id}`)       
 
         const newReplyLi = document.createElement("li")
         newReplyLi.id = "reply-new"
+        newReplyLi.addEventListener("click", submitReply)
         
         const username = replyLi.querySelector(".username") as HTMLSpanElement
         
@@ -18,7 +28,7 @@ export const createReplyToReply = (e: Event) => {
                 
         newReplyCreator.classList.add("new-reply-window")
         newReplyCreator.innerHTML = `
-            <textarea class="textarea" maxlength="120">@${username.innerText}</textarea>
+            <textarea class="textarea" maxlength="120">@${replyRef[0].user.username}</textarea>
             <div class ="bottom-reply-container">
                 <img src="${loggedUser.image.png}" alt="avatar"></img>
                 <button class="submit-reply-btn">REPLY</button>
--- src/functions/SubmitReply.ts
+++ src/functions/SubmitReply.ts
@@ -6,13 +6,13 @@ import { Comments, Replies } from "../components/Types"
 
 export const submitReply = (e: Event) => {
     //submit button
-    const submitReplyButton = e.target as Element
+    const submitReplyButton = e.target as Element    
 
     //element with event target
-    const tempLi = e.currentTarget as Element
+    const tempLi = e.currentTarget as Element    
 
     //catching whole comment we're replying to
-    const commentLi = tempLi.parentElement.closest("li") 
+    const commentLi = tempLi.parentElement.closest("li")
     
     //textarea
     const textarea = tempLi.querySelector(".textarea") as HTMLTextAreaElement    
@@ -24,40 +24,94 @@ export const submitReply = (e: Event) => {
 
     const commentToReply: Array<Comments> = commentsToObject.filter(comment => commentLi.id === `comment-${comment.id}`)
 
-    if (submitReplyButton.classList.contains("submit-reply-btn")) {
-               
-        const newReply: Replies = {
-            id: newReplyId,
-                content: textarea.value.split(" ").slice(1).join(" "),
-                createdAt: "1 week ago",
-                score: 0,
-                replyingTo: commentToReply[0].user.username,
-                user: {
-                    image: { 
-                    png: loggedUser.image.png,
-                    webp: loggedUser.image.webp
+    if (submitReplyButton.classList.contains("submit-reply-btn")) { 
+        if (tempLi.closest("li").previousElementSibling === null) {
+            const newReply: Replies = {
+                id: newReplyId,
+                    content: textarea.value.split(" ").slice(1).join(" "),
+                    createdAt: "1 week ago",
+                    score: 0,
+                    replyingTo: commentToReply[0].user.username,
+                    user: {
+                        image: { 
+                        png: loggedUser.image.png,
+                        webp: loggedUser.image.webp
+                        },
+                        username: loggedUser.username
                     },
-                    username: loggedUser.username
-                },
-        }
-            commentToReply[0].replies.push(newReply)
-
-        const newReplyLi: HTMLLIElement = renderReply(newReply)
-
-        if (commentLi.querySelector(".replies-container") === null) {
-            const newRepliesContainer: HTMLUListElement = document.createElement("ul")
-            newRepliesContainer.classList.add("replies-container")
-            newRepliesContainer.append(newReplyLi)
-            commentLi.append(newRepliesContainer)
+            }
             
-        }   else {
-            const existingRepliesContainer: HTMLDivElement = commentLi.querySelector(".replies-container")
-            existingRepliesContainer.append(newReplyLi)          
+            commentToReply[0].replies.push(newReply)
+    
+            const newReplyLi: HTMLLIElement = renderReply(newReply)
+    
+            if (commentLi.querySelector(".replies-container") === null) {
+                const newRepliesContainer: HTMLUListElement = document.createElement("ul")
+                newRepliesContainer.classList.add("replies-container")
+                newRepliesContainer.append(newReplyLi)
+                commentLi.append(newRepliesContainer)
+                
+            }   else {
+                const existingRepliesContainer: HTMLDivElement = commentLi.querySelector(".replies-container")
+                existingRepliesContainer.append(newReplyLi)          
+            }
+            
+            const commentsToJSON: string = JSON.stringify(commentsToObject)
+            localStorage.setItem("comments", commentsToJSON)
+    
+            if (!tempLi.parentElement.classList.contains("replies-container")) {
+                tempLi.parentElement.remove()
+    
+            } else {
+                tempLi.remove()
+            }
+
+        } else {
+            const searchedReply = commentToReply[0].replies.filter(reply => {
+                return tempLi.closest("li").previousElementSibling.id === `reply-${reply.id}`
+            })
+
+            const newReply: Replies = {
+                id: newReplyId,
+                    content: textarea.value.split(" ").slice(1).join(" "),
+                    createdAt: "1 week ago",
+                    score: 0,
+                    replyingTo: searchedReply[0].user.username,
+                    user: {
+                        image: { 
+                        png: loggedUser.image.png,
+                        webp: loggedUser.image.webp
+                        },
+                        username: loggedUser.username
+                    },
+            }
+            
+            commentToReply[0].replies.push(newReply)
+    
+            const newReplyLi: HTMLLIElement = renderReply(newReply)
+    
+            if (commentLi.querySelector(".replies-container") === null) {
+                const newRepliesContainer: HTMLUListElement = document.createElement("ul")
+                newRepliesContainer.classList.add("replies-container")
+                newRepliesContainer.append(newReplyLi)
+                commentLi.append(newRepliesContainer)
+                
+            }   else {
+                const existingRepliesContainer: HTMLDivElement = commentLi.querySelector(".replies-container")
+                existingRepliesContainer.append(newReplyLi)          
+            }
+            
+            const commentsToJSON: string = JSON.stringify(commentsToObject)
+            localStorage.setItem("comments", commentsToJSON)
+    
+            if (!tempLi.parentElement.classList.contains("replies-container")) {
+                tempLi.parentElement.remove()
+    
+            } else {
+                tempLi.remove()
+            }
         }
         
-        const commentsToJSON: string = JSON.stringify(commentsToObject)
-        localStorage.setItem("comments", commentsToJSON)
-        tempLi.parentElement.remove()
     }
 
     commentLi.addEventListener("click", createReplyToComment)
